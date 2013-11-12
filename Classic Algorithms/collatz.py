f59807e2bd6387d92715e539ca16b8e1bb8591b8
Daniel O'Prey <d@oprey.co.uk>
2013-11-12 10:24:54 +0800

    complete collatz.py

--- Classic Algorithms/collatz.py
+++ Classic Algorithms/collatz.py
@@ -1,19 +1,43 @@
 # by Daniel O'Prey
 
-def collatz(num):
-    return None
+def collatz(num, steps):
+    """
+    Find the number of steps it takes to reach one using the following process: If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1.
+    """
+    
+    if num == 1:
+        print('Completed in', steps, 'steps')
+        inp_user()
+    
+    steps += 1
+    
+    if num % 2 == 0:
+        print('Step', steps, ': ', int(num), 'is even, dividing by 2')
+        num /= 2
+        collatz(num, steps)
+    else:
+        print('Step', steps, ': ', int(num), 'is odd, multiplying by 3 and adding 1')
+        num *= 3
+        num += 1
+        collatz(num, steps)
 
 def inp_user():
     """
     Takes an input from the user, exception handles until its an integer and then calls fib_digits
     """
 
-    num = input('How many digits? ')
-    if type(num) == int and num > 1:
-        collatz(num)
-    else:
-        print('Please input an integer which is larger than one')
+    try:
+        num = int(input('Please enter a number larger than 1: '))
+        if num > 1:
+            collatz(num, 0)
+        else:
+            print('The number must be larter than 1')
+            inp_user()
+    except ValueError:
+        print('Please enter a number larger than 1')
         inp_user()
+    except:
+        exit()
         
 if __name__ == '__main__':
     inp_user()

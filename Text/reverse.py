7f47124df33c8624bec27a7aee8094dae2153ec0
Daniel O'Prey <d@oprey.co.uk>
2013-11-12 13:50:09 +0800

    add defin

--- Text/reverse.py
+++ Text/reverse.py
@@ -1,6 +1,10 @@
 # by Daniel O'Prey
 
 def reverse_string():
+	"""
+	Take a string the user entered and print it in reverse
+	"""
+
     print(input('What text would you like to reverse? ')[::-1])
     reverse_string()
 

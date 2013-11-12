7069f211f556812ca5452ef85f6f4489f3357bd9
Daniel O'Prey <d@oprey.co.uk>
2013-11-12 14:06:08 +0800

    fix indent

--- Text/reverse.py
+++ Text/reverse.py
@@ -1,9 +1,9 @@
 # by Daniel O'Prey
 
 def reverse_string():
-	"""
-	Take a string the user entered and print it in reverse
-	"""
+    """
+    Take a string the user entered and print it in reverse
+    """
 
     print(input('What text would you like to reverse? ')[::-1])
     reverse_string()

574f52e80010d3c1a1e20be85d3446467c842b30
Alex <alexmolineux88@alex-molineuxs-macbook.local>
2013-12-03 23:53:06 +0000

    completed factorization

--- Numbers/prime_factors.py
+++ Numbers/prime_factors.py
@@ -1,16 +1,13 @@
 #Prime Factors
 
-
-
 def prime_factors():
     user_num = input("Enter a number: ")
     results = []
     n, i = user_num, 2
-    while i * i < n:
+    for i in range(2, n):
         while n % i == 0:
             results.append(i)
-            n = n / i
-        i += 1
+            n /= i
     return results
     
 print prime_factors()
